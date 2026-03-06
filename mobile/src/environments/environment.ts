e41e189b90d5379f55b4fa85b5046d3bf6019a6a
Francis AHONSOU <45388225+AQUILA04@users.noreply.github.com>
2026-03-06 14:58:38 +0400

    fix prod accounting date

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.126:8081',
+  apiUrl: 'http://localhost:8081',
   appName: 'Elykia',
-  version: '2.6.0'
+  version: '2.6.2'
 };
 
 /*
