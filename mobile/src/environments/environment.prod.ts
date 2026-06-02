94172e3e9c22966ac69c7269adf67953a4ab53d8
ericdzik <ahonsueric01@gmail.com>
2026-06-02 07:17:40 +0000

    ip local 2

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://192.168.1.72:8081',
+  apiUrl: 'http://192.168.1.72:8081',
   appName: 'Elykia',
   version: '2.7.5'
 };
--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://192.168.1.72:8081',
+  apiUrl: 'http://192.168.1.72:8081',
   appName: 'Elykia',
   version: '2.7.5'
 };
