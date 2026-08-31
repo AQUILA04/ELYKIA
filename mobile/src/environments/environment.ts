1a7f527b57c4d59147cc014f427b7b232145022b
ericdzik <ahonsueric01@gmail.com>
2026-08-31 12:22:14 +0000

    test ip local 3

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.72:8081',
+  apiUrl: 'http://192.168.1.72:8081',//ip local
   appName: 'Elykia',
   version: '2.28.8'
 };
